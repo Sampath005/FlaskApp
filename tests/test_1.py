4ff61dac54bedbeedc5ab6c1bcdab222402dec08
Sampath005 <sampath300500@gmail.com>
2024-01-29 17:39:56 +0530

    initial commit

---
+++ tests/test_1.py
@@ -0,0 +1,13 @@
+import pytest
+from src.app import app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_hello(client):
+    response = client.get('/')
+    assert b'Hello, World!' in response.data
