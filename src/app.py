4342cc4fef53cf7205fd2fa09ed51856f5609830
Sampath005 <97429122+Sampath005@users.noreply.github.com>
2024-02-28 09:52:02 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello():
     return 'Hello, world!'
 
+@app.route('/sam')
+def hello():
+    return 'Hello, Sam!'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
